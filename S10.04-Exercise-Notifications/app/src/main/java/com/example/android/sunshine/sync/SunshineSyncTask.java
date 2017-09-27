0518424ad27d33ea18f3a63d2bc73991405673cc
Bogdan Orzea <bogdan.orzea@gmail.com>
2017-09-27 22:48:52 +0300

    S10.04

--- S10.04-Exercise-Notifications/app/src/main/java/com/example/android/sunshine/sync/SunshineSyncTask.java
+++ S10.04-Exercise-Notifications/app/src/main/java/com/example/android/sunshine/sync/SunshineSyncTask.java
@@ -18,14 +18,19 @@ package com.example.android.sunshine.sync;
 import android.content.ContentResolver;
 import android.content.ContentValues;
 import android.content.Context;
+import android.text.format.DateUtils;
 
+import com.example.android.sunshine.data.SunshinePreferences;
 import com.example.android.sunshine.data.WeatherContract;
 import com.example.android.sunshine.utilities.NetworkUtils;
+import com.example.android.sunshine.utilities.NotificationUtils;
 import com.example.android.sunshine.utilities.OpenWeatherJsonUtils;
 
 import java.net.URL;
 
 public class SunshineSyncTask {
+
+    private static long ellapsedTimeSinceLastNotification;
 
     /**
      * Performs the network request for updated weather, parses the JSON from that request, and
@@ -73,12 +78,22 @@ public class SunshineSyncTask {
                         WeatherContract.WeatherEntry.CONTENT_URI,
                         weatherValues);
 
-//              TODO (13) Check if notifications are enabled
+                // COMPLETED (13) Check if notifications are enabled
+                boolean notificationEnabled = SunshinePreferences.isNotificationEnabled(context);
 
-//              TODO (14) Check if a day has passed since the last notification
+                // COMPLETED (14) Check if a day has passed since the last notification
+                boolean notificationSentBeforeToday = false;
+                ellapsedTimeSinceLastNotification =
+                        SunshinePreferences.getEllapsedTimeSinceLastNotification(context);
 
-//              TODO (15) If more than a day have passed and notifications are enabled, notify the user
+                if (ellapsedTimeSinceLastNotification >= DateUtils.DAY_IN_MILLIS){
+                    notificationSentBeforeToday = true;
+                }
 
+//              COMPLETED (15) If more than a day have passed and notifications are enabled, notify the user
+                if (notificationEnabled && notificationSentBeforeToday){
+                    NotificationUtils.notifyUserOfNewWeather(context);
+                }
             /* If the code reaches this point, we have successfully performed our sync */
 
             }
--- S10.04-Exercise-Notifications/app/src/main/java/com/example/android/sunshine/utilities/NotificationUtils.java
+++ S10.04-Exercise-Notifications/app/src/main/java/com/example/android/sunshine/utilities/NotificationUtils.java
@@ -1,14 +1,22 @@
 package com.example.android.sunshine.utilities;
 
 
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.Context;
+import android.content.Intent;
 import android.content.res.Resources;
 import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
+import android.support.v4.app.NotificationCompat;
+import android.support.v4.app.TaskStackBuilder;
 
+import com.example.android.sunshine.DetailActivity;
+import com.example.android.sunshine.MainActivity;
 import com.example.android.sunshine.R;
+import com.example.android.sunshine.data.SunshinePreferences;
 import com.example.android.sunshine.data.WeatherContract;
 
 public class NotificationUtils {
@@ -32,7 +40,9 @@ public class NotificationUtils {
     public static final int INDEX_MAX_TEMP = 1;
     public static final int INDEX_MIN_TEMP = 2;
 
-//  TODO (1) Create a constant int value to identify the notification
+    // COMPLETED (1) Create a constant int value to identify the notification
+    private static final int NOTIFICATION_ID = 19;
+
 
     /**
      * Constructs and displays a notification for the newly updated weather for today.
@@ -83,19 +93,38 @@ public class NotificationUtils {
             int smallArtResourceId = SunshineWeatherUtils
                     .getSmallArtResourceIdForWeatherCondition(weatherId);
 
-//          TODO (2) Use NotificationCompat.Builder to begin building the notification
+//          COMPLETED (2) Use NotificationCompat.Builder to begin building the notification
+            NotificationCompat.Builder builder = new NotificationCompat.Builder(context);
 
-//          TODO (3) Create an Intent with the proper URI to start the DetailActivity
+//          COMPLETED (3) Create an Intent with the proper URI to start the DetailActivity
+            Intent intentDetailActivity = new Intent(context, DetailActivity.class);
+            intentDetailActivity.setData(todaysWeatherUri);
 
-//          TODO (4) Use TaskStackBuilder to create the proper PendingIntent
+//          COMPLETED (4) Use TaskStackBuilder to create the proper PendingIntent
+            TaskStackBuilder stackBuilder = TaskStackBuilder.create(context);
+            stackBuilder.addNextIntentWithParentStack(intentDetailActivity);
 
-//          TODO (5) Set the content Intent of the NotificationBuilder
+            PendingIntent pendingIntent = stackBuilder
+                    .getPendingIntent(0, PendingIntent.FLAG_UPDATE_CURRENT);
 
-//          TODO (6) Get a reference to the NotificationManager
+//          COMPLETED (5) Set the content Intent of the NotificationBuilder
+            builder.setContentIntent(pendingIntent)
+                    .setContentText(notificationText)
+                    .setContentTitle(notificationTitle)
+                    .setSmallIcon(smallArtResourceId)
+                    .setLargeIcon(largeIcon)
+                    .setAutoCancel(true)
+                    .setPriority(NotificationCompat.PRIORITY_MAX);
 
-//          TODO (7) Notify the user with the ID WEATHER_NOTIFICATION_ID
+//          COMPLETED (6) Get a reference to the NotificationManager
+            NotificationManager notificationManager = (NotificationManager) context.
+                    getSystemService(Context.NOTIFICATION_SERVICE);
 
-//          TODO (8) Save the time at which the notification occurred using SunshinePreferences
+//          COMPLETED (7) Notify the user with the ID WEATHER_NOTIFICATION_ID
+            notificationManager.notify(NOTIFICATION_ID, builder.build());
+
+//          COMPLETED (8) Save the time at which the notification occurred using SunshinePreferences
+            SunshinePreferences.saveLastNotificationTime(context, System.currentTimeMillis());
         }
 
         /* Always close your cursor when you're done with it to avoid wasting resources. */
